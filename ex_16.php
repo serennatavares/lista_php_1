7182a0c83543de539b482ae2dd0b029882e5625b
serennadias <serenna_tavares@estudante.sesisenai.org.br>
2026-08-05 12:53:27 -0300

    add - inicio exercicio 16

---
+++ ex_16.php
@@ -0,0 +1,25 @@
+<?php
+
+function analisarSenha(){
+
+}
+
+function letrasMaiusculas(){
+
+}
+
+function contarNumeros(){
+
+}
+
+function contarCaracteresEspeciais(){
+
+}
+
+function classificarSenha(){
+
+}
+
+
+
+?>
