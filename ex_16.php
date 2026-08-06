66fc13352e50cadb679a409f7230fd0e222cd39d
SerennaDias <serenna_tavares@estudante.sesisenai.org.br>
2026-08-06 15:24:36 -0300

    add - exercicio 16

--- ex_16.php
+++ ex_16.php
@@ -1,25 +1,73 @@
 <?php
 
-function analisarSenha(){
-
+function letrasMaiusculas($senha){
+    preg_match_all('/[A-Z]/', $senha, $resultado);
+    return count($resultado[0]);
 }
 
-function letrasMaiusculas(){
-
+function letrasMinusculas($senha){
+    preg_match_all('/[a-z]/', $senha, $resultado);
+    return count($resultado[0]);
 }
 
-function contarNumeros(){
-
+function contarNumeros($senha){
+    preg_match_all('/[0-9]/', $senha, $resultado);
+    return count($resultado[0]);
 }
 
-function contarCaracteresEspeciais(){
-
+function contarCaracteresEspeciais($senha){
+    preg_match_all('/[^a-zA-Z0-9]/', $senha, $resultado);
+    return count($resultado[0]);
 }
 
-function classificarSenha(){
+function classificarSenha($senha){
 
+    $tamanho = strlen($senha);
+    $maiusculas = letrasMaiusculas($senha);
+    $minusculas = letrasMinusculas($senha);
+    $numeros = contarNumeros($senha);
+    $especiais = contarCaracteresEspeciais($senha);
+
+    $criterios = 0;
+
+    if ($tamanho >= 8) $criterios++;
+    if ($maiusculas > 0) $criterios++;
+    if ($minusculas > 0) $criterios++;
+    if ($numeros > 0) $criterios++;
+    if ($especiais > 0) $criterios++;
+
+    if ($criterios <= 2){
+        return "Fraca";
+    } elseif ($criterios == 3){
+        return "Média";
+    } elseif ($criterios == 4){
+        return "Forte";
+    } else {
+        return "Muito forte";
+    }
 }
 
+function analisarSenha($senha){
 
+    return [
+        "maiusculas" => letrasMaiusculas($senha),
+        "minusculas" => letrasMinusculas($senha),
+        "numeros" => contarNumeros($senha),
+        "especiais" => contarCaracteresEspeciais($senha),
+        "tamanho" => strlen($senha),
+        "nivel" => classificarSenha($senha)
+    ];
+}
+
+$senha = "labubu@hikaru1114";
+
+$resultado = analisarSenha($senha);
+
+echo "Maiúsculas: " . $resultado["maiusculas"] . "<br>";
+echo "Minúsculas: " . $resultado["minusculas"] . "<br>";
+echo "Números: " . $resultado["numeros"] . "<br>";
+echo "Especiais: " . $resultado["especiais"] . "<br>";
+echo "Tamanho: " . $resultado["tamanho"] . "<br>";
+echo "Nível: " . $resultado["nivel"] . "<br>";
 
 ?>
