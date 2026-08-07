544a1775d2f3bd143c303c8bb35fc1585dc3b3d0
serennadias <serenna_tavares@estudante.sesisenai.org.br>
2026-08-07 11:20:55 -0300

    fix - exercicio 1

--- ex_01.php
+++ ex_01.php
@@ -1,22 +1,28 @@
 <?php
 
-// (X² + Y²) / (X + Y)
+// Uma empresa de engenharia precisa automatizar alguns cálculos utilizados em seus projetos.
+// Crie uma função chamada calcularFormula() que receba dois números e aplique a seguinte fórmula:
+// (x²+y²) / (x + y)
+// Retorne ao resultado da operação.
+// Caso a soma seja igual a zero, informe que não é possível realizar a divisão.
 
-function calcularFormula($x, $y)
-{
-    if (($x + $y) == 0) {
-        return "Não é possível realizar a divisão por zero.";
+function calcularFormula($x,$y){
+
+    if (($x + $y) == 0){
+        return "Não foi possível realizar a divisão por zero!";
     }
+    // (x²+y²) / (x + y)
 
-    $resultado = (pow($x, 2) + pow($y, 2)) / ($x = $y);
-
+    $resultado = ((pow($x,2) + pow($y,2)) / ($x + $y));
     return $resultado;
 
 }
+$x_usuario = 30;
+$y_usuario = 50;
 
-$x = 10;
-$y = 5;
+echo "Valor de X é: $x_usuario <br>";
+echo "Valor de Y é: $y_usuario <br>";
 
-echo "Valor de X: $x <br>";
-echo "Valor de Y: $y <br><br>";
-echo "Resultado: " . calcularFormula($x, $y);
+echo calcularFormula($x_usuario,$y_usuario);
+
+?>
